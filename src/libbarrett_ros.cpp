a42ef610fde6de2f0154cf1c125ae6485bb624c7
Michael Koval <mkoval@cs.cmu.edu>
2015-05-06 18:42:21 -0400

    Load multiple configuration files.

--- src/libbarrett_ros.cpp
+++ src/libbarrett_ros.cpp
@@ -28,8 +28,11 @@
 #include <vector>
 #include <boost/array.hpp>
 #include <boost/format.hpp>
+#include <boost/foreach.hpp>
+#include <boost/filesystem/operations.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/shared_ptr.hpp>
+#include <barrett/config.h>
 #include <barrett/exception.h>
 #include <barrett/systems.h>
 
@@ -49,51 +52,98 @@
 #include <libbarrett_ros/ForceTorqueSensorHW.h>
 #include <libbarrett_ros/WamHW.h>
 
+using ::barrett::ProductManager;
 using ::libbarrett_ros::BarrettRobotHW;
 using ::libbarrett_ros::ForceTorqueSensorHW;
 using ::libbarrett_ros::WamHW;
+using ::XmlRpc::XmlRpcValue;
 
-int main(int argc, char **argv)
+static std::vector<std::string> get_configuration_paths(
+    XmlRpcValue &configs_xmlrpc
+  )
 {
-  BarrettRobotHW robot;
+  using ::boost::format;
+  using ::boost::str;
+  using ::boost::filesystem::is_directory;
+  using ::boost::filesystem::is_regular_file;
 
-  // Initialize ROS.
-  ::ros::init(argc, argv, "libbarrett_ros");
-  ::ros::NodeHandle nh;
-  ::ros::AsyncSpinner spinner(2);
-  spinner.start();
+  std::vector<std::string> configs;
 
-  // Read parameters.
-  bool wait_for_shift_activate = true;
-  bool prompt_on_zeroing = true;
+  switch (configs_xmlrpc.getType()) {
+  case XmlRpcValue::TypeArray:
+    for (int i = 0; i < configs_xmlrpc.size(); ++i) {
+      XmlRpcValue &config_xmlrpc = configs_xmlrpc[i];
 
-  nh.getParam("wait_for_shift_activate", wait_for_shift_activate);
-  nh.getParam("prompt_on_zeroing", prompt_on_zeroing);
+      if (config_xmlrpc.getType() == XmlRpcValue::TypeString) {
+        configs.push_back(config_xmlrpc);
+      } else {
+        throw std::runtime_error("Configuration file path is not a string.");
+      }
+    }
+    break;
 
-  // Initialize libbarrett.
-  ::barrett::installExceptionHandler();
-  ::barrett::ProductManager pm;
+  case XmlRpcValue::TypeString:
+    configs.push_back(configs_xmlrpc);
+    break;
 
-  // TODO: Is this the right initialization order?
+  case XmlRpcValue::TypeInvalid:
+    configs.push_back(::barrett::EtcPathRelative("default.conf"));
+    break;
+
+  default:
+    throw std::runtime_error("Invalid configurations.");
+  }
+
+  // Run some sanity checks to catch common mistakes.
+  for (size_t i = 0; i < configs.size(); ++i) {
+    std::string const &config_path = configs[i];
+
+    if (is_directory(config_path)) {
+      throw std::runtime_error(str(
+        format("Configuration path '%s' is a directory, but should be a file."
+               " Did you forget to include 'default.conf'?") % config_path));
+    } else if (!is_regular_file(config_path)) {
+      throw std::runtime_error(str(
+        format("Configuration path '%s' is not a file.") % config_path));
+    }
+  }
+
+  return configs;
+}
+
+static void initialize_product_manager(
+    ProductManager &pm,
+    BarrettRobotHW &robot
+  )
+{
+  using ::boost::make_shared;
+
+  // TODO: Can we safely set both of these to false?
+  static bool const prompt_on_zeroing = true;
+  static bool const wait_for_shift_activate = true;
+
+  // TODO: Can we defer some of this to later?
   pm.waitForWam(prompt_on_zeroing);
   pm.wakeAllPucks();
-  pm.getSafetyModule()->waitForMode(barrett::SafetyModule::IDLE);
+
+  // TODO: What does this do?
+  pm.getSafetyModule()->waitForMode(::barrett::SafetyModule::IDLE);
 
   // TODO: Don't hard-code joint names.
   if (pm.foundWam7()) {
     ROS_INFO("Found a 7-DOF WAM.");
     robot.add(
-      boost::make_shared<WamHW<7> >(
+      make_shared<WamHW<7> >(
         pm.getWam7(wait_for_shift_activate)));
   } else if (pm.foundWam4()) {
     ROS_INFO("Found a 4-DOF WAM.");
     robot.add(
-      boost::make_shared<WamHW<4> >(
+      make_shared<WamHW<4> >(
         pm.getWam4(wait_for_shift_activate)));
   } else if (pm.foundWam3()) {
     ROS_INFO("Found a 3-DOF WAM.");
     robot.add(
-      boost::make_shared<WamHW<3> >(
+      make_shared<WamHW<3> >(
         pm.getWam3(wait_for_shift_activate)));
   }
 
@@ -108,20 +158,61 @@ int main(int argc, char **argv)
     ROS_INFO("Found a force/torque sensor.");
     // TODO: Don't hard-code the name and frame_id.
     robot.add(
-      boost::make_shared<ForceTorqueSensorHW>(
+      make_shared<ForceTorqueSensorHW>(
         pm.getForceTorqueSensor(), "ft_sensor", "ft_sensor_frame"));
+  }
+}
+
+int main(int argc, char **argv)
+{
+  using ::boost::make_shared;
+  using ::boost::shared_ptr;
+
+  ::barrett::installExceptionHandler();
+
+  // Initialize ROS.
+  ::ros::init(argc, argv, "libbarrett_ros");
+  ::ros::NodeHandle nh;
+  ::ros::AsyncSpinner spinner(2);
+  spinner.start();
+
+  // Read parameters.
+  ::XmlRpc::XmlRpcValue configs_xmlrpc;
+  std::vector<std::string> config_paths;
+
+  nh.getParam("configurations", configs_xmlrpc);
+
+  try {
+    config_paths = get_configuration_paths(configs_xmlrpc);
+    ROS_INFO_STREAM("Loading " << config_paths.size() << " config files.");
+  } catch (std::runtime_error const &e) {
+    ROS_FATAL("Failed loading configurations: %s", e.what());
+    return 1;
+  }
+
+  // Initialize libbarrett.
+  BarrettRobotHW robot;
+  std::vector<shared_ptr<ProductManager> > product_managers;
+  product_managers.reserve(config_paths.size());
+
+  BOOST_FOREACH (std::string const &config_path, config_paths) {
+    ROS_INFO_STREAM("Loading configuration file '" << config_path << "'.");
+
+    shared_ptr<ProductManager> const product_manager
+      = make_shared<ProductManager>(config_path.c_str());
+
+    initialize_product_manager(*product_manager, robot);
+    product_managers.push_back(product_manager);
   }
 
   // Initialize ros_control.
   robot.initialize();
   ::controller_manager::ControllerManager cm(&robot);
 
-  // Start the control loop.
-  ::barrett::systems::RealTimeExecutionManager *execution_manager
-    = pm.getExecutionManager();
-  ros::Duration const period(execution_manager->getPeriod());
+  // Start the control loop. Default to 500 Hz.
+  // TODO: Figure out threading and real-time safety.
+  ros::Duration const period(0, 500000000);
 
-  // TODO: Can we run this inside the RealTimeExecutionManager?
   while (ros::ok()) {
     robot.read();
 
