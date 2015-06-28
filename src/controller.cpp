f20d1c82b57aebf41c0ff51b8810ae76fe5aaadc
Michael Koval <mkoval@cs.cmu.edu>
2015-06-28 01:47:25 -0500

    Working on RT-safety.

--- src/controller.cpp
+++ src/controller.cpp
@@ -3,9 +3,10 @@
 #include <barrett/exception.h>
 #include <controller_manager/controller_manager.h>
 #include <libbarrett_ros/params.h>
+#include <libbarrett_ros/BarrettRobotHW.h>
 #include <libbarrett_ros/Bus.h>
 #include <libbarrett_ros/BusInfo.h>
-#include <libbarrett_ros/BarrettRobotHW.h>
+#include <libbarrett_ros/ControlLoop.h>
 
 int main(int argc, char **argv)
 {
@@ -19,10 +20,6 @@ int main(int argc, char **argv)
   // Initialize ROS.
   ros::init(argc, argv, "libbarrett_ros_controller");
   ros::NodeHandle nh;
-
-  // TODO: Should we use an AsyncSpinner?
-  ros::AsyncSpinner spinner(2);
-  spinner.start();
 
   // Read parameters.
   // TODO: Is this the right way to get the private parameter namespace?
@@ -49,6 +46,8 @@ int main(int argc, char **argv)
     = get_or_default<int>(root_xmlrpc, "control_period", 2000); // 500 Hz
   uint_fast32_t const bus_freq
     = get_or_default<int>(root_xmlrpc, "bus_frequency", 1e6); // 1 Mbps
+  double const publish_freq 
+    = get_or_default<double>(root_xmlrpc, "publish_frequency", 100); // 100 Hz
 
   // Initialize the communication buses.
   std::vector<Bus> buses(bus_infos.size());
@@ -100,30 +99,38 @@ int main(int argc, char **argv)
   ROS_INFO("Initializing the ControllerManager.");
   controller_manager::ControllerManager cm(&robot);
 
-  // Start the control loop. Default to 500 Hz.
-  // TODO: ros::Rate and ros::Time are likely not realtime-safe.
+  // Start the control loop in a separate (potentially realtime) thread.
+  // TODO: This may not work if libbarrett was built with Xenomai support (i.e.
+  // -DNON_REALTIME=false), but this controller was run with "realtime" ROS
+  // parameter set to "false". ControLoop uses the barrett::PeriodicLoopTimer
+  // to run a realtime control thread, but we will be passing "realtime=false"
+  // to many libbarrett functions (e.g. Puck::getProperty). This will cause the
+  // control thread to drop out of primary mode and produce Xenomai warnings.
   ROS_INFO("Entering control loop.");
+  ControlLoop loop(&buses, &cm, control_period, 10);
+  loop.Start();
 
-  // TODO: This ros::Rate instance ignores the period.
-  ros::Duration const period(0, control_period * 1e3);
-  ros::Rate r(500);
+  // Use the main (non-realtime) thread to process ROS messages. This function
+  // will exit when the user presses Control+C.
+  // TODO: Could the Control+C signal to be caught by the control thread?
+  // TODO: Should we use an AsyncSpinner for this?
+  ros::Rate rate(publish_freq);
 
-  while (ros::ok()) {
-    BOOST_FOREACH (Bus &bus, buses) {
-      bus.schedule.RunPreControl();
-    }
-
-    ros::Time const now(::barrett::highResolutionSystemTime());
-    cm.update(now, period);
-
-    BOOST_FOREACH (Bus &bus, buses) {
-      bus.schedule.RunPostControl();
-    }
-
-    r.sleep();
+  while (ros::ok() && loop.is_running()) {
+    ros::spinOnce();
+    rate.sleep();
   }
 
-  ROS_INFO("Exited control loop.");
+  // Interrupt the control loop and wait the control thread to exit. This is a
+  // no-op if the thread already has stopped.
+  loop.Stop();
+
+  if (loop.has_error()) {
+    ROS_ERROR_STREAM("Control loop exited with error: "
+                     << loop.error_message());
+  } else {
+    ROS_INFO("Exited control loop.");
+  }
 
   // Idle the hardware to avoid triggering a heartbeat fault.
   ROS_INFO("Idling hardware.");
