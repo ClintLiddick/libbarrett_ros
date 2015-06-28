5fd5cce547f1cd58d1eb39ac05faba06055116a7
Michael Koval <mkoval@cs.cmu.edu>
2015-06-28 00:05:04 -0500

    Renamed the executable to "controller".

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,93 +1,26 @@
-/*
- Copyright 2012 Barrett Technology <support@barrett.com>
-
- This file is part of libbarrett_ros.
-
- This version of libbarrett_ros is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
-
- This version of libbarrett_ros is distributed in the hope that it will be
- useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License along
- with this version of libbarrett_ros.  If not, see
- <http://www.gnu.org/licenses/>.
-
- Barrett Technology holds all copyrights on libbarrett_ros. As the sole
- copyright holder, Barrett reserves the right to release future versions
- of libbarrett_ros under a different license.
-
- File: libbarrett_ros.cpp
- Date: 15 October, 2014
- Author: Hariharasudan Malaichamee
- */
-#include <vector>
-#include <boost/array.hpp>
-#include <boost/format.hpp>
+#include <ros/ros.h>
 #include <boost/foreach.hpp>
-#include <boost/assign/list_of.hpp>
-#include <boost/filesystem/operations.hpp>
-#include <boost/make_shared.hpp>
-#include <boost/shared_ptr.hpp>
 #include <barrett/exception.h>
-#include <barrett/systems.h>
-
-#include "ros/ros.h"
-#include <urdf/model.h>
-#include <iostream>
-#include <vector>
-#include <string>
-#include <memory>
-#include <barrett/products/product_manager.h>
-#include <boost/thread.hpp>
-#include <barrett/systems/real_time_execution_manager.h>
-#include <hardware_interface/robot_hw.h>
 #include <controller_manager/controller_manager.h>
-
-#include <realtime_tools/realtime_clock.h>
-#include <libbarrett_ros/TaskSet.h> // for TaskSet
-#include <libbarrett_ros/BarrettRobotHW.h>
-#include <libbarrett_ros/ForceTorqueSensorHW.h>
-#include <libbarrett_ros/HandHW.h>
-#include <libbarrett_ros/WamHW.h>
 #include <libbarrett_ros/params.h>
-#include <libbarrett_ros/BusInfo.h>
 #include <libbarrett_ros/Bus.h>
-#include <libbarrett_ros/Schedule.h>
-
-using barrett::ProductManager;
-using libbarrett_ros::BarrettBaseHW;
-using libbarrett_ros::BarrettRobotHW;
-using libbarrett_ros::ForceTorqueSensorHW;
-using libbarrett_ros::Schedule;
-using libbarrett_ros::WamHW;
-using libbarrett_ros::BusInfo;
-using libbarrett_ros::get_value;
-using libbarrett_ros::get_or_throw;
-using libbarrett_ros::get_or_default;
-using libbarrett_ros::Task;
-using libbarrett_ros::TaskSet;
-using libbarrett_ros::Cycle;
-using libbarrett_ros::CycleInfo;
-using libbarrett_ros::ScheduleInfo;
-using libbarrett_ros::Bus;
-using libbarrett_ros::InitializeBus;
-using XmlRpc::XmlRpcValue;
+#include <libbarrett_ros/BusInfo.h>
+#include <libbarrett_ros/BarrettRobotHW.h>
 
 int main(int argc, char **argv)
 {
-  using ::boost::make_shared;
-  using ::boost::shared_ptr;
+  using namespace libbarrett_ros;
 
-  // TODO: What does this do?
+  // Installs a new terminate() function that, when no catch clauses handle an
+  // exception, prints a stacktrace to stderr before performing the default
+  // behavior (terminating the process and generating a core file).
   barrett::installExceptionHandler();
 
   // Initialize ROS.
-  ros::init(argc, argv, "barrett_ros");
+  ros::init(argc, argv, "libbarrett_ros_controller");
   ros::NodeHandle nh;
+
+  // TODO: Should we use an AsyncSpinner?
   ros::AsyncSpinner spinner(2);
   spinner.start();
 
@@ -112,11 +45,10 @@ int main(int argc, char **argv)
     return 1;
   }
 
-  // Default to a 500 Hz conrol frequency on a 1 Mbps CAN bus.
   uint_fast32_t const control_period
-    = get_or_default<int>(root_xmlrpc, "control_period", 2000);
+    = get_or_default<int>(root_xmlrpc, "control_period", 2000); // 500 Hz
   uint_fast32_t const bus_freq
-    = get_or_default<int>(root_xmlrpc, "bus_frequency", 1e6);
+    = get_or_default<int>(root_xmlrpc, "bus_frequency", 1e6); // 1 Mbps
 
   // Initialize the communication buses.
   std::vector<Bus> buses(bus_infos.size());
@@ -172,6 +104,7 @@ int main(int argc, char **argv)
   // TODO: ros::Rate and ros::Time are likely not realtime-safe.
   ROS_INFO("Entering control loop.");
 
+  // TODO: This ros::Rate instance ignores the period.
   ros::Duration const period(0, control_period * 1e3);
   ros::Rate r(500);
 
